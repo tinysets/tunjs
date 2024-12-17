cff9a588dd11342b2acfcbf0c3f005215a5ff8ed
zhaolihang-mac <635335512@qq.com>
2024-12-17 23:45:29 +0800

    export api  1.0.4

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+export * from './Common/interfaces'
+export * from './Common/Msg'
+export * from './Common/Pipe'
+export * from './Common/TCPPacket'
+export * from './Common/TunnelInfo'
+export * from './Common/Utils'
 
+export * from './Socket/TCPClient'
+export * from './Socket/TCPServer'
+export * from './Socket/UDPClient'
+export * from './Socket/UDPClient'
+
+export * from './Tunnel/LocalTunnel'
+export * from './Tunnel/TunnelManager'
+export * from './Tunnel/VirtualEndPoint'
+
+export * from './Client'
+export * from './Server'
